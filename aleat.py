6a65693a185b9a687c0d10a57b3068ffb20ca343
SheilaOM <sheila.oliva14@gmail.com>
2017-02-20 16:16:15 +0100

    Corregido tras comprobar con check

--- aleat.py
+++ aleat.py
@@ -1,12 +1,14 @@
 import webapp
 
+
 class urlaleat(webapp.webApp):
 
     def process(self, parsedRequest):
         import random
-        num = random.randint(1,1000000)
+        num = random.randint(1, 1000000)
 
-        return ("200 OK", "<html><body><h1><a href='" + str(num) + "'>Dame mas</h1></body></html>")
+        return ("200 OK", "<html><body><h1><a href='" + str(num) +
+                "'>Dame mas</h1></body></html>")
 
 if __name__ == "__main__":
     testWebApp = urlaleat("localhost", 1234)
